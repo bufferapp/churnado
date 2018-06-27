783733b199ba99fac0d396c73dbae8460b6b5fbd
David Gasquez <davidgasquez@gmail.com>
2018-06-27 09:02:15 +0200

    Use user id present in subscription table

--- dbt/models/churnado_subscriptions.sql
+++ dbt/models/churnado_subscriptions.sql
@@ -1,6 +1,7 @@
 select
   s.id as subscription_id
   , s.customer_id
+  , s.metadata_user_id as user_id
   , s.plan_id
   , s.simplified_plan_id
   , s.canceled_at
@@ -8,7 +9,6 @@ select
   , s.quantity
   , s.trial_start_at
   , s.trial_end_at
-  , u.id as user_id
   , case -- if subscription was active on Dec 31 and was created two months before, we can use it in the training set
       when s.created_at < '2017-11-01' and (s.canceled_at is null or s.canceled_at > '2017-12-31')
       then true
@@ -18,8 +18,6 @@ select
   , count(distinct case when c.refunded then c.id else null end) as refunded_charges
   , count(distinct case when c.paid = false and c.captured = false then c.id else null end) as failed_charges
 from dbt.stripe_subscriptions as s
-inner join dbt.users as u
-  on s.customer_id = u.billing_stripe_customer_id
 left join dbt.stripe_invoices as i -- join invoices and charges created before Dec 31
   on i.subscription_id = s.id
   and i.paid
